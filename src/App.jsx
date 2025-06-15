5b96fd7bc43588923c193e7eb4297ccceb19609b
PixelG <emrekudu57@gmail.com>
2025-06-15 21:35:01 +0200

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AdminPanel from "./AdminPanel";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AdminPanel />
-  </React.StrictMode>
-);
